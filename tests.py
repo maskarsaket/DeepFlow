91637216dc433372b8ae1cb6b08c602004e86661
maskarasaket <maskarasaket@gmail.com>
2020-04-26 10:04:50 +0530

    Updated folder structure

---
+++ tests.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from DeepFlow import DeepFlow 
+from time import sleep
+import random
+import os
+
+projectname = "DeepFlow"
+runmaster = "runmaster.csv"
+bench = 90
+
+print("Description : ")
+desc = input()
+
+print("ParentID : ")
+parentID = int(input())
+
+params = {
+    'model' : 'LGB',
+    'Lagslist' : '1,2,3'
+}
+
+flow = DeepFlow(
+    projectname = projectname,
+    runmasterfile = runmaster,
+    description = desc,
+    parentID = parentID,
+    benchmark = bench,
+    params = params
+)
+
+print("RMSE : ")
+
+flow.log_score('RMSE', float(input()))
+sleep(2)
+
+imp = pd.DataFrame({
+    'Feature':['test1', 'test2'],
+    'Importance':[1,2]
+})
+
+path = 'Runs/exp1'
+
+flow.log_imp(imp, 'Runs/exp1')
+
+flow.end_run()
