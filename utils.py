0c75dc50ee3ad6012757e1b6c27714cb3853ca31
maskarasaket <maskarasaket@gmail.com>
2020-05-02 00:21:07 +0530

    Added dashed line for benchmark in journey plot

--- utils.py
+++ utils.py
@@ -132,77 +132,6 @@ def create_feature_imp_plot(df, graphid, text, hovertemplate="Count : %{text}"):
                 config={"displayModeBar": False},
             )
 
-def create_journey_plot(dfrunmaster):
-    return dcc.Graph(
-            id="graph-2",
-            figure={
-                "data": [
-                    go.Scatter(
-                        x=dfrunmaster[dfrunmaster.Chosen==1]['ExpID'],
-                        y=dfrunmaster[dfrunmaster.Chosen==1]['Score'],
-                        text=dfrunmaster[dfrunmaster.Chosen==1]['Description'],
-                        mode="lines+markers",
-                        name="Chosen",
-                        hovertemplate= "Exp%{x}<br>%{text}<br>" + "Score : %{y}"
-                    ),
-                    go.Scatter(
-                        x=dfrunmaster[dfrunmaster.Chosen==0]['ExpID'],
-                        y=dfrunmaster[dfrunmaster.Chosen==0]['Score'],
-                        text=dfrunmaster[dfrunmaster.Chosen==0]['Description'],
-                        mode="markers",
-                        name="Not Chosen",
-                        hovertemplate= "Exp%{x}<br>%{text}<br>" + "Score : %{y}"
-                    ),
-                ],
-                "layout": go.Layout(
-                    autosize=True,
-                    title="",
-                    font={"family": "Raleway", "size": 10},
-                    height=250,
-                    # width=340,
-                    hovermode="closest",
-                    hoverlabel={"font_family": "Raleway", "font_size": 10},
-                    legend={
-                        "x": -0.0277108433735,
-                        "y": -0.142606516291,
-                        "orientation": "h",
-                    },
-                    margin={
-                        "r": 20,
-                        "t": 20,
-                        "b": 20,
-                        "l": 50,
-                    },
-                    showlegend=True,
-                    xaxis={
-                        "autorange": True,
-                        "linecolor": "rgb(0, 0, 0)",
-                        "linewidth": 1,
-                        "showgrid": False,
-                        "showline": True,
-                        "title": "",
-                        # "type": "linear",
-                        "zeroline" : False
-                    },
-                    yaxis={
-                        "autorange": True,
-                        "gridcolor": "rgba(127, 127, 127, 0.2)",
-                        "mirror": False,
-                        "nticks": 4,
-                        "showgrid": True,
-                        "showline": True,
-                        "ticklen": 10,
-                        "ticks": "outside",
-                        "title": "Score",
-                        "type": "linear",
-                        "zeroline": False,
-                        "zerolinewidth": 4,
-                    },
-                ),
-            },
-            config={"displayModeBar": False},
-        )
-
 def create_journey_plot_new(dfrunmaster):
     fig = px.line(
             data_frame=dfrunmaster[dfrunmaster.Chosen==1],
@@ -278,64 +207,83 @@ def create_journey_plot_new(dfrunmaster):
         )
 
 def create_journey_plot_line(dfrunmaster):
+    figure = go.Figure()
+
+    ### Add traces for scores
+    for scoretype in dfrunmaster.ScoreType.unique():
+        figure.add_traces([
+            go.Scatter(
+                x=dfrunmaster[dfrunmaster.ScoreType==scoretype]['ExpID'],
+                y=dfrunmaster[dfrunmaster.ScoreType==scoretype]['Score'],
+                text=dfrunmaster[dfrunmaster.ScoreType==scoretype]['Description'],
+                mode="lines+markers",
+                name=scoretype,
+                hovertemplate= "%{text}<br>" + "<b>Score : <b>%{y}<br>"
+            )
+        ])
+
+    ### Add trace for benchmark
+    figure.add_traces([
+        go.Scatter(
+            x=dfrunmaster['ExpID'],
+            y=dfrunmaster['Benchmark'],
+            line={'dash':'dash'},
+            name="Benchmark",
+            hovertemplate="Benchmark %{y}"
+        )
+    ])
+    
+    figure.update_layout(
+        go.Layout(
+            autosize=True,
+            title="",
+            font={"family": "Raleway", "size": 10},
+            height=250,
+            # width=340,
+            hovermode="closest",
+            hoverlabel={"font_family": "Raleway", "font_size": 10},
+            legend={
+                "x": -0.0277108433735,
+                "y": -0.142606516291,
+                "orientation": "h",
+            },
+            margin={
+                "r": 20,
+                "t": 20,
+                "b": 20,
+                "l": 50,
+            },
+            showlegend=True,
+            xaxis={
+                "autorange": True,
+                "linecolor": "rgb(0, 0, 0)",
+                "linewidth": 1,
+                "showgrid": False,
+                "showline": True,
+                "title": "",
+                # "type": "linear",
+                "zeroline" : False
+            },
+            yaxis={
+                "autorange": True,
+                "gridcolor": "rgba(127, 127, 127, 0.2)",
+                "mirror": False,
+                "nticks": 4,
+                "showgrid": True,
+                "showline": True,
+                "ticklen": 10,
+                "ticks": "outside",
+                "title": "Score",
+                "type": "linear",
+                "zeroline": False,
+                "zerolinewidth": 4,
+            }
+        ),
+        overwrite=True
+    )
+
     return dcc.Graph(
             id="graph-2",
-            figure={
-                "data": [
-                    dict(
-                        x=dfrunmaster[dfrunmaster.ScoreType==i]['ExpID'],
-                        y=dfrunmaster[dfrunmaster.ScoreType==i]['Score'],
-                        text=dfrunmaster[dfrunmaster.ScoreType==i]['Description'],
-                        mode="line",
-                        name=i,
-                        hovertemplate= "%{text}<br>" + "<b>Score : <b>%{y}<br>"
-                    ) for i in dfrunmaster.ScoreType.unique()
-                ],
-                "layout": go.Layout(
-                    autosize=True,
-                    title="",
-                    font={"family": "Raleway", "size": 10},
-                    height=250,
-                    # width=340,
-                    hovermode="closest",
-                    hoverlabel={"font_family": "Raleway", "font_size": 10},
-                    legend={
-                        "x": -0.0277108433735,
-                        "y": -0.142606516291,
-                        "orientation": "h",
-                    },
-                    margin={
-                        "r": 20,
-                        "t": 20,
-                        "b": 20,
-                        "l": 50,
-                    },
-                    showlegend=True,
-                    xaxis={
-                        "autorange": True,
-                        "linecolor": "rgb(0, 0, 0)",
-                        "linewidth": 1,
-                        "showgrid": False,
-                        "showline": True,
-                        "title": "",
-                        # "type": "linear",
-                        "zeroline" : False
-                    },
-                    yaxis={
-                        "autorange": True,
-                        "gridcolor": "rgba(127, 127, 127, 0.2)",
-                        "mirror": False,
-                        "nticks": 4,
-                        "showgrid": True,
-                        "showline": True,
-                        "ticklen": 10,
-                        "ticks": "outside",
-                        "title": "Score",
-                        "type": "linear",
-                        "zeroline": False,
-                        "zerolinewidth": 4,
-                    },
-                ),
-            },
+            figure=figure,
             config={"displayModeBar": False},
         )
